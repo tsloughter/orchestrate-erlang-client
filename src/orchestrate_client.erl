9e61b2cbd0c4b6755f867f9a33e0f893a1defe8b
Chris Molozian <chris@cmoz.me>
2014-02-24 18:57:35 +0000

    Remove excessive guard clauses and don't use '++' to concat string lists.

--- src/orchestrate_client.erl
+++ src/orchestrate_client.erl
@@ -25,6 +25,7 @@
          relation_put/5,
          relation_purge/5]).
 
+%% TODO add specs to the public code
 %% TODO escape all parameters for URI-encoding
 %% TODO remove code duplication when building common URIs
 %% TODO do better error handling
@@ -32,124 +33,107 @@
 
 %% API
 
-delete(Collection) when is_bitstring(Collection); is_list(Collection) ->
+delete(Collection) ->
     request(delete, Collection, [], []).
 
-search(Collection, LuceneQuery) when is_bitstring(Collection); is_list(Collection) ->
-    UriFragment = Collection ++ "?query=" ++ LuceneQuery,
+search(Collection, LuceneQuery) ->
+    UriFragment = [Collection, "?query=", LuceneQuery],
     request(get, UriFragment, [], []).
 
-search(Collection, LuceneQuery, Offset) when 
-        is_bitstring(Collection); is_list(Collection),
-        is_bitstring(LuceneQuery); is_list(LuceneQuery),
-        is_integer(Offset), Offset >= 0
-        ->
-    UriFragment = Collection ++ "?query=" ++ LuceneQuery ++ "&offset=" ++ Offset,
+search(Collection, LuceneQuery, Offset) ->
+    UriFragment = [Collection, "?query=", LuceneQuery, "&offset=", Offset],
     request(get, UriFragment, [], []).
 
-search(Collection, LuceneQuery, Offset, Limit) when
-        is_bitstring(Collection); is_list(Collection),
-        is_bitstring(LuceneQuery); is_list(LuceneQuery),
-        is_integer(Offset); Offset >= 0,
-        is_integer(Limit); Limit >= 0
-        ->
-    UriFragment = Collection ++ "?query=" ++ LuceneQuery ++ "&offset=" ++ Offset ++ "&limit=" ++ Limit,
+search(Collection, LuceneQuery, Offset, Limit) ->
+    UriFragment = [Collection, "?query=", LuceneQuery, "&offset=", Offset, "&limit=", Limit],
     request(get, UriFragment, [], []).
 
-kv_get(Collection, Key) when is_bitstring(Collection); is_list(Collection), is_bitstring(Key); is_list(Key) ->
-    UriFragment = Collection ++ "/" ++ Key,
+kv_get(Collection, Key) ->
+    UriFragment = [Collection, "/", Key],
     request(get, UriFragment, [], []).
 
 kv_get(Collection, Key, Ref) ->
-    UriFragment = Collection ++ "/" ++ Key ++ "/refs/" ++ Ref,
+    UriFragment = [Collection, "/", Key, "/refs/", Ref],
     request(get, UriFragment, [], []).
 
-kv_list(Collection) when is_bitstring(Collection); is_list(Collection) ->
+kv_list(Collection) ->
     request(get, Collection, [], []).
 
 kv_list(Collection, Limit) when is_integer(Limit) ->
-    UriFragment = Collection ++ "?limit=" ++ Limit,
+    UriFragment = [Collection, "?limit=", Limit],
     request(get, UriFragment, [], []);
 kv_list(Collection, StartKey) ->
-    UriFragment = Collection ++ "?startKey=" ++ StartKey,
+    UriFragment = [Collection, "?startKey=", StartKey],
     request(get, UriFragment, [], []).
 
+kv_list(Collection, StartKey, true) ->
+    UriFragment = [Collection, "?afterKey=", StartKey],
+    request(get, UriFragment, [], []);
 kv_list(Collection, StartKey, Limit) when is_integer(Limit) ->
-    UriFragment = Collection ++ "?startKey=" ++ StartKey ++ "&limit=" ++ Limit,
-    request(get, UriFragment, [], []);
-kv_list(Collection, StartKey, Inclusive) when Inclusive =:= true ->
-    UriFragment = Collection ++ "?afterKey=" ++ StartKey,
+    UriFragment = [Collection, "?startKey=", StartKey, "&limit=", Limit],
     request(get, UriFragment, [], []).
 
-kv_list(Collection, StartKey, Limit, Inclusive) when Inclusive =:= true ->
-    UriFragment = Collection ++ "?afterKey=" ++ StartKey ++ "&limit=" ++ Limit,
+kv_list(Collection, StartKey, Limit, true) ->
+    UriFragment = [Collection, "?afterKey=", StartKey, "&limit=", Limit],
     request(get, UriFragment, [], []);
-kv_list(Collection, StartKey, Limit, Inclusive) when Inclusive =:= false ->
-    UriFragment = Collection ++ "?startKey=" ++ StartKey ++ "&limit" ++ Limit,
+kv_list(Collection, StartKey, Limit, _) ->
+    UriFragment = [Collection, "?startKey=", StartKey, "&limit", Limit],
     request(get, UriFragment, [], []).
 
 kv_put(Collection, Key, Body) ->
-    UriFragment = Collection ++ "/" ++ Key,
+    UriFragment = [Collection, "/", Key],
     request(put, UriFragment, [], Body).
 
-kv_put(Collection, Key, Body, Match) when is_boolean(Match) ->
-    UriFragment = Collection ++ "/" ++ Key,
-    Headers = [
-        {"if-none-match", Match}
-    ],
-    request(put, UriFragment, Headers, Body);
-kv_put(Collection, Key, Body, Match) when is_bitstring(Match); is_list(Match) ->
-    UriFragment = Collection ++ "/" ++ Key,
-    Headers = [
-        {"if-match", Match}
-    ],
+kv_put(Collection, Key, Body, Match) ->
+    UriFragment = [Collection, "/", Key],
+    Headers = case Match of
+        true ->
+            [{"if-none-match", Match}];
+        _ ->
+            [{"if-match", Match}]
+    end,
     request(put, UriFragment, Headers, Body).
 
 kv_delete(Collection, Key) ->
-    UriFragment = Collection ++ "/" ++ Key,
+    UriFragment = [Collection, "/", Key],
     request(delete, UriFragment, [], []).
 
-kv_delete(Collection, Key, IfMatch) when is_list(IfMatch) ->
-    UriFragment = Collection ++ "/" ++ Key,
+kv_delete(Collection, Key, IfMatch) ->
+    UriFragment = [Collection, "/", Key],
     Headers = [
         {"if-match", IfMatch}
     ],
     request(delete, UriFragment, Headers, []).
 
 kv_purge(Collection, Key) ->
-    UriFragment = Collection ++ "/" ++ Key ++ "?purge=true",
+    UriFragment = [Collection, "/", Key, "?purge=true"],
     request(delete, UriFragment, [], []).
 
-kv_purge(Collection, Key, IfMatch) when is_list(IfMatch) ->
-    UriFragment = Collection ++ "/" ++ Key ++ "?purge=true",
+kv_purge(Collection, Key, IfMatch) ->
+    UriFragment = [Collection, "/", Key, "?purge=true"],
     Headers = [
         {"if-match", IfMatch}
     ],
     request(delete, UriFragment, Headers, []).
 
 events_get(Collection, Key, EventType) ->
-    % TODO add some guard clauses
-    UriFragment = Collection ++ "/" ++ Key ++ "/events/" ++ EventType,
+    UriFragment = [Collection, "/", Key, "/events/", EventType],
     request(get, UriFragment, [], []).
 
 events_get(Collection, Key, EventType, Start) ->
-    % TODO add some guard clauses
-    UriFragment = Collection ++ "/" ++ Key ++ "/events/" ++ EventType ++ "?start=" ++ Start,
+    UriFragment = [Collection, "/", Key, "/events/", EventType, "?start=", Start],
     request(get, UriFragment, [], []).
 
 events_get(Collection, Key, EventType, Start, End) ->
-    % TODO add some guard clauses
-    UriFragment = Collection ++ "/" ++ Key ++ "/events/" ++ EventType ++ "?start=" ++ Start ++ "&end=" ++ End,
+    UriFragment = [Collection, "/", Key, "/events/", EventType, "?start=", Start, "&end=", End],
     request(get, UriFragment, [], []).
 
 events_put(Collection, Key, EventType) ->
-    % TODO add some guard clauses
-    UriFragment = Collection ++ "/" ++ Key ++ "/events/" ++ EventType,
+    UriFragment = [Collection, "/", Key, "/events/", EventType],
     request(put, UriFragment, [], []).
 
 events_put(Collection, Key, EventType, Timestamp) ->
-    % TODO add some guard clauses
-    UriFragment = Collection ++ "/" ++ Key ++ "/events/" ++ EventType ++ "?timestamp=" ++ Timestamp,
+    UriFragment = [Collection, "/", Key, "/events/", EventType, "?timestamp=", Timestamp],
     request(put, UriFragment, [], []).
 
 relation_get(Collection, Key, Kinds) ->
@@ -158,19 +142,17 @@ relation_get(Collection, Key, Kinds) ->
     {error, "not yet implemented."}.
 
 relation_put(Collection, Key, Kind, ToCollection, ToKey) ->
-    % TODO add some guard clauses
-    UriFragment = Collection ++ "/" ++ Key ++ "/relation/" ++ Kind ++ "/" ++ ToCollection ++ "/" ++ ToKey,
+    UriFragment = [Collection, "/", Key, "/relation/", Kind, "/", ToCollection, "/", ToKey],
     request(put, UriFragment, [], []).
 
 relation_purge(Collection, Key, Kind, ToCollection, ToKey) ->
-    % TODO add some guard clauses
-    UriFragment = Collection ++ "/" ++ Key ++ "/relation/" ++ Kind ++ "/" ++ ToCollection ++ "/" ++ ToKey ++ "?purge=true",
+    UriFragment = [Collection, "/", Key, "/relation/", Kind, "/", ToCollection, "/", ToKey, "?purge=true"],
     request(delete, UriFragment, [], []).
 
 %% Internal API
 
 request(Method, UriFragment, Headers, Body) ->
-    Url = "https://api.orchestrate.io:443/v0/" ++ UriFragment,
+    Url = ["https://api.orchestrate.io:443/v0/", UriFragment],
     % TODO move API key to a "client" record
     % TODO expose some of the HttpOptions
     AuthHeaderValue = "Basic " ++ base64:encode_to_string("4390ea15-947e-4948-9984-9ba2c1d508a5:"),
