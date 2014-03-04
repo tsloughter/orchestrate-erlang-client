149fef4b993ed31d2d45173105bfb701b701c000
Chris Molozian <chris@cmoz.me>
2014-03-04 07:20:06 +0000

    Fix a couple of TODOs in the client. Merged #4.

--- src/orchestrate_client.erl
+++ src/orchestrate_client.erl
@@ -1,6 +1,7 @@
 -module(orchestrate_client).
 
--export([delete/1,
+-export([set_apikey/1,
+         delete/1,
          search/2,
          search/3,
          search/4,
@@ -26,66 +27,72 @@
          relation_purge/5]).
 
 %% TODO add specs to the public code
-%% TODO escape all parameters for URI-encoding
-%% TODO remove code duplication when building common URIs
 %% TODO do better error handling
 %% TODO add an eunit test suite
 
 %% API
 
+set_apikey(ApiKey) when is_list(ApiKey) ->
+    set_apikey(list_to_binary(ApiKey));
+set_apikey(ApiKey) when is_binary(ApiKey) ->
+    EncodedToken = base64:encode(<<ApiKey/binary, ":">>),
+    Auth = <<"Basic ", EncodedToken/binary>>,
+    application:set_env(orchestrate_client, http_auth, binary_to_list(Auth)).
+
 delete(Collection) ->
     request(delete, Collection, [], []).
 
 search(Collection, LuceneQuery) ->
-    UriFragment = [Collection, "?query=", LuceneQuery],
+    UriFragment = search_uri_encode([Collection], LuceneQuery),
     request(get, UriFragment, [], []).
 
 search(Collection, LuceneQuery, Offset) ->
-    UriFragment = [Collection, "?query=", LuceneQuery, "&offset=", Offset],
+    UriFragment = [search_uri_encode([Collection], LuceneQuery), "&offset=", Offset],
     request(get, UriFragment, [], []).
 
 search(Collection, LuceneQuery, Offset, Limit) ->
-    UriFragment = [Collection, "?query=", LuceneQuery, "&offset=", Offset, "&limit=", Limit],
+    UriFragment = [search_uri_encode([Collection], LuceneQuery), "&offset=", Offset, "&limit=", Limit],
     request(get, UriFragment, [], []).
 
 kv_get(Collection, Key) ->
-    UriFragment = [Collection, "/", Key],
+    UriFragment = uri_path_encode([Collection, Key]),
     request(get, UriFragment, [], []).
 
 kv_get(Collection, Key, Ref) ->
-    UriFragment = [Collection, "/", Key, "/refs/", Ref],
+    UriFragment = [uri_path_encode([Collection, Key]), "/refs/", Ref],
     request(get, UriFragment, [], []).
 
 kv_list(Collection) ->
-    request(get, Collection, [], []).
+    UriFragment = uri_path_encode([Collection]),
+    request(get, UriFragment, [], []).
 
 kv_list(Collection, Limit) when is_integer(Limit) ->
-    UriFragment = [Collection, "?limit=", Limit],
+    UriFragment = [uri_path_encode([Collection]), "?limit=", Limit],
     request(get, UriFragment, [], []);
 kv_list(Collection, StartKey) ->
-    UriFragment = [Collection, "?startKey=", StartKey],
+    UriFragment = kv_list_uri_encode([Collection], "startKey", StartKey),
     request(get, UriFragment, [], []).
 
 kv_list(Collection, StartKey, true) ->
-    UriFragment = [Collection, "?afterKey=", StartKey],
+    UriFragment = kv_list_uri_encode([Collection], "afterKey", StartKey),
     request(get, UriFragment, [], []);
 kv_list(Collection, StartKey, Limit) when is_integer(Limit) ->
-    UriFragment = [Collection, "?startKey=", StartKey, "&limit=", Limit],
+    UriFragment = [kv_list_uri_encode([Collection], "startKey", StartKey), "&limit=", Limit],
     request(get, UriFragment, [], []).
 
 kv_list(Collection, StartKey, Limit, true) ->
-    UriFragment = [Collection, "?afterKey=", StartKey, "&limit=", Limit],
+    UriFragment = [kv_list_uri_encode([Collection], "afterKey", StartKey), "&limit=", Limit],
     request(get, UriFragment, [], []);
 kv_list(Collection, StartKey, Limit, _) ->
-    UriFragment = [Collection, "?startKey=", StartKey, "&limit", Limit],
+    UriFragment = [kv_list_uri_encode([Collection], "startKey", StartKey), "&limit", Limit],
     request(get, UriFragment, [], []).
 
 kv_put(Collection, Key, Body) ->
-    UriFragment = [Collection, "/", Key],
+    UriFragment = uri_path_encode([Collection, Key]),
     request(put, UriFragment, [], Body).
 
 kv_put(Collection, Key, Body, Match) ->
-    UriFragment = [Collection, "/", Key],
+    UriFragment = uri_path_encode([Collection, Key]),
     Headers = case Match of
         true ->
             [{"if-none-match", Match}];
@@ -95,72 +102,82 @@ kv_put(Collection, Key, Body, Match) ->
     request(put, UriFragment, Headers, Body).
 
 kv_delete(Collection, Key) ->
-    UriFragment = [Collection, "/", Key],
+    UriFragment = uri_path_encode([Collection, Key]),
     request(delete, UriFragment, [], []).
 
 kv_delete(Collection, Key, IfMatch) ->
-    UriFragment = [Collection, "/", Key],
+    UriFragment = uri_path_encode([Collection, Key]),
     Headers = [
         {"if-match", IfMatch}
     ],
     request(delete, UriFragment, Headers, []).
 
 kv_purge(Collection, Key) ->
-    UriFragment = [Collection, "/", Key, "?purge=true"],
+    UriFragment = [uri_path_encode([Collection, Key]), "?purge=true"],
     request(delete, UriFragment, [], []).
 
 kv_purge(Collection, Key, IfMatch) ->
-    UriFragment = [Collection, "/", Key, "?purge=true"],
+    UriFragment = [uri_path_encode([Collection, Key]), "?purge=true"],
     Headers = [
         {"if-match", IfMatch}
     ],
     request(delete, UriFragment, Headers, []).
 
 events_get(Collection, Key, EventType) ->
-    UriFragment = [Collection, "/", Key, "/events/", EventType],
+    UriFragment = events_uri_encode([Collection, Key], EventType),
     request(get, UriFragment, [], []).
 
 events_get(Collection, Key, EventType, Start) ->
-    UriFragment = [Collection, "/", Key, "/events/", EventType, "?start=", Start],
+    UriFragment = [events_uri_encode([Collection, Key], EventType), "?start=", Start],
     request(get, UriFragment, [], []).
 
 events_get(Collection, Key, EventType, Start, End) ->
-    UriFragment = [Collection, "/", Key, "/events/", EventType, "?start=", Start, "&end=", End],
+    UriFragment = [events_uri_encode([Collection, Key], EventType), "?start=", Start, "&end=", End],
     request(get, UriFragment, [], []).
 
 events_put(Collection, Key, EventType) ->
-    UriFragment = [Collection, "/", Key, "/events/", EventType],
+    UriFragment = events_uri_encode([Collection, Key], EventType),
     request(put, UriFragment, [], []).
 
 events_put(Collection, Key, EventType, Timestamp) ->
-    UriFragment = [Collection, "/", Key, "/events/", EventType, "?timestamp=", Timestamp],
+    UriFragment = [events_uri_encode([Collection, Key], EventType), "?timestamp=", Timestamp],
     request(put, UriFragment, [], []).
 
 relation_get(Collection, Key, Kinds) ->
-    % TODO add some guard clauses
-    % TODO implement this
-    {error, "not yet implemented."}.
+    UriFragment = uri_path_encode([Collection, Key, "relations", Kinds]),
+    request(get, UriFragment, [], []).
 
 relation_put(Collection, Key, Kind, ToCollection, ToKey) ->
-    UriFragment = [Collection, "/", Key, "/relation/", Kind, "/", ToCollection, "/", ToKey],
+    UriFragment = uri_path_encode([Collection, Key, "relation", Kind, ToCollection, ToKey]),
     request(put, UriFragment, [], []).
 
 relation_purge(Collection, Key, Kind, ToCollection, ToKey) ->
-    UriFragment = [Collection, "/", Key, "/relation/", Kind, "/", ToCollection, "/", ToKey, "?purge=true"],
+    UriFragment = [uri_path_encode([Collection, Key, "relation", Kind, ToCollection, ToKey]), "?purge=true"],
     request(delete, UriFragment, [], []).
 
 %% Internal API
 
+kv_list_uri_encode(PathFragments, KeyTypeQueryParam, StartKey) ->
+    [uri_path_encode(PathFragments), "?", KeyTypeQueryParam, "=", StartKey].
+
+events_uri_encode(PathFragments, EventType) ->
+    [uri_path_encode(PathFragments), "/events/", http_uri:encode(EventType)].
+
+search_uri_encode(PathFragments, LuceneQuery) ->
+    [uri_path_encode(PathFragments), "?query=", http_uri:encode(LuceneQuery)].
+
+uri_path_encode(PathFragments) ->
+    [ ["/", http_uri:encode(Path)] || Path <- PathFragments ].
+
 request(Method, UriFragment, Headers, Body) ->
-    Url = lists:flatten("https://api.orchestrate.io:443/v0/", UriFragment),
-    % TODO move API key to a "client" record
+    Url = lists:flatten(["https://api.orchestrate.io:443/v0", UriFragment]),
+    {ok, Auth} = application:get_env(orchestrate_client, http_auth),
     % TODO expose some of the HttpOptions
-    AuthHeaderValue = lists:flatten("Basic ", base64:encode_to_string("4390ea15-947e-4948-9984-9ba2c1d508a5:")),
-    Headers2 = [
+    Headers2 = Headers ++ [
         {"accept", "application/json"},
-        {"authorization", AuthHeaderValue},
+        {"authorization", Auth},
         {"user-agent", "Orchestrate Java Client/0.2.0"}
-    ] ++ Headers,
+    ],
     case Method of
         put ->
             Type = "application/json",
@@ -174,6 +191,10 @@ request(Method, UriFragment, Headers, Body) ->
         {connect_timeout, 1000}
     ],
     Options = [],
+    % TODO don't set these options on every request
+    httpc:set_options([
+        {pipeline_timeout, 60000}
+    ]),
     Response = httpc:request(Method, Request, HttpOptions, Options),
     case Response of
         {ok, {{_, Status, _}, Headers3, Body2}} when Status >= 200, Status < 400 ->
